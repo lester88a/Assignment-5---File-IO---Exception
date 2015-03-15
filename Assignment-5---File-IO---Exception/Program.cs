a78a3a0df242344c352e1c4bea5e9f73e8c4b205
lester88a <lester88a@gmail.com>
2015-03-15 13:50:53 -0400

    Created WriteFile ReadFile Method

--- Assignment-5---File-IO---Exception/Program.cs
+++ Assignment-5---File-IO---Exception/Program.cs
@@ -11,9 +11,21 @@ namespace Assignment_5___File_IO___Exception
     {
         static void Main(string[] args)
         {
+            
+            MenuMethod();
+
+        }
+
+        //Menu Method++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private static void MenuMethod()
+        {
+            string pathName = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName; //declear the current path as a pathName
+            string fileName = "GradeFile.txt"; //declear the file name
+
+
             int choice = 0; //declear local variable for choice
-           
-            while (choice!=2) //while loop menu
+
+            while (choice != 2) //while loop menu
             {
                 Console.WriteLine("+++++++++++++++++++++++++++");
                 Console.WriteLine("+           Menu          +");
@@ -21,7 +33,7 @@ namespace Assignment_5___File_IO___Exception
                 Console.WriteLine("+     2.Exit              +");
                 Console.WriteLine("+++++++++++++++++++++++++++");
                 Console.Write("Enter your choice: ");
-                
+
                 try
                 {
                     choice = Convert.ToInt32(Console.ReadLine());
@@ -29,13 +41,14 @@ namespace Assignment_5___File_IO___Exception
                 catch (Exception error)
                 {
                     choice = 0;
-                    //Console.WriteLine(error.Message);
+                    Console.WriteLine(error.Message);
                 }
                 switch (choice)
                 {
                     case 1:
-                       
-                        
+                        CheckFile();
+                        WriteFileMethod(pathName,fileName);
+                        ReadFileMethod(pathName, fileName);
                         break;
                     case 2://select to exit
                         Console.WriteLine();
@@ -44,7 +57,7 @@ namespace Assignment_5___File_IO___Exception
                         Console.WriteLine();
                         Console.WriteLine("Incorrect input, please try again!");
                         Console.WriteLine();
-                        WaitForKey();
+                        WaitForKey(); //if incorrect input, then display wait for key info
                         break;
                 }
                 Console.Clear(); //clear the screen
@@ -52,7 +65,96 @@ namespace Assignment_5___File_IO___Exception
             }
         }
 
-        // UTILITY METHODS
+        //CheckFile Method++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private static void CheckFile()
+        {
+            string prompt;
+            Console.Write("Please enter a file name: ");
+            prompt = Console.ReadLine();
+            Console.WriteLine();
+
+            if (File.Exists(prompt))
+            {
+                Console.WriteLine("The File Exists");
+                Console.WriteLine();
+                Console.WriteLine("File Stats:");
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++");
+                Console.WriteLine("Create Time: " + File.GetCreationTime(prompt));
+                Console.WriteLine("Last Access: " + File.GetLastAccessTime(prompt));
+                Console.WriteLine("Last Write : " + File.GetLastWriteTime(prompt));
+                Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++");
+
+            }
+            else
+            {
+                Console.WriteLine("No such file");
+            }
+            WaitForKey();
+        }
+
+
+        //WriteFile Method++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private static void WriteFileMethod(string pathName, string fileName)
+        {
+
+            try
+            {
+                FileStream outFile = new FileStream(pathName + fileName, FileMode.Create, FileAccess.Write); //created a outFile in current path
+                StreamWriter writer = new StreamWriter(outFile); //declear a wirter variable
+
+                //data variable
+                string[] firstName = { "Jones", "Johnson", "Smith" };
+                string[] lastName = { "Bob", "Sarah", "Sam" };
+                int[] ID = { 1, 2, 3 };
+                string[] classes = { "Introduction to Computer Science", "Data Structures", "Data Structures" };
+                string[] grade = { "A-", "B+", "C" };
+
+                //wite data to the outFIle
+                for (int i = 0; i < 3; i++)
+                {
+                    writer.WriteLine("{0}, {1}: {2} {3}, {4}", firstName[i], lastName[i], ID[i], classes[i], grade[i]);
+                }
+
+                writer.Close(); // closes the file
+                outFile.Close(); // closes the  file stream
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Your code caused a darn error!!!");
+                Console.WriteLine("Error: {0} ", error.Message);
+            }
+        }
+
+        //ReadFile Method++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private static void ReadFileMethod(string pathName, string fileName)
+        {
+            string fileData = "";
+            string[] fileArray = new string[10];
+            try
+            {
+                FileStream inFile = new FileStream(pathName + fileName, FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(inFile);
+
+                for (int row = 0; row < 10; row++)
+                {
+                    fileData = reader.ReadLine();
+                    fileArray[row] = fileData;
+
+                    Console.WriteLine("Your Data: {0}", fileData);
+                } // Read one record (line of data)
+                reader.Close(); // closes the file
+                inFile.Close(); // closes the  file stream
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Your code caused a darn error!!!");
+                Console.WriteLine("Error: {0} ", error.Message);
+
+            }
+        }
+
+
+        // UTILITY METHODS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         private static void WaitForKey()
         {
             Console.WriteLine();
@@ -62,5 +164,8 @@ namespace Assignment_5___File_IO___Exception
             Console.ReadKey();
             Console.Clear();
         }
+
+
+
     }
 }
